40ba0dd5ac7eb18da56e9d2b62ebbca69ab8bb00
Anna Kosyhina <77195959+Misiokinka@users.noreply.github.com>
2024-06-02 16:03:24 +0300

    1

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -1,76 +1,63 @@
-import * as fs from "node:fs/promises";
-import path from "node:path";
-import { nanoid } from "nanoid";
-
-const contactsPath = path.resolve("db", "contacts.json");
-
-
-export default async function listContacts() {
-    const data = await fs.readFile(contactsPath, { encoding: "utf8" });
-
-    return JSON.parse(data);
-}
-async function writeContacts(contacts) {
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, undefined, 2));
-}
-export async function getContactById(contactId) {
-    const contacts = await listContacts();
-    const contact = contacts.find((contact) => contact.id === contactId);
-    if (typeof contact === "undefined") {
-        return null;
-    }
-    return contact;
-}
+import Contact from "../models/contacts.js";
 
 export async function getContacts() {
-    return await listContacts();
+    try {
+        return await Contact.find();
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
+
+export async function getContactById(contactId) {
+    try {
+        const contact = await Contact.findById(contactId);
+        return contact ? contact : null;
+    } catch (error) {
+        throw new Error(error.message);
+    }
 }
 
 export async function removeContact(contactId) {
-    const contacts = await listContacts();
-
-    const index = contacts.findIndex((contact) => contact.id === contactId);
-    if (index === -1) {
-        return null;
+    try {
+        const contact = await Contact.findByIdAndDelete(contactId);
+        return contact ? contact : null;
+    } catch (error) {
+        throw new Error(error.message);
     }
-
-    const removedContact = contacts[index];
-
-    contacts.splice(index, 1);
-
-    await writeContacts(contacts);
-
-    return removedContact;
 }
 
-export async function addContact(newContact) {
-    const contacts = await listContacts();
-
-    let newContactItems = {
-        id: nanoid(),
-        ...newContact,
-
+export async function addContact(name, email, phone, favorite = false) {
+    const newContact = {
+        name,
+        email,
+        phone,
+        favorite,
     };
-
-    contacts.push(newContactItems);
-    await writeContacts(contacts);
-
-    return newContactItems;
+    try {
+        return await Contact.create(newContact);
+    } catch (error) {
+        throw new Error(error.message);
+    }
 }
 
 export async function updateContacts(id, data) {
-    const contacts = await listContacts();
-    const index = contacts.findIndex(item => item.id === id);
-
-    if (index === -1) {
-        return null;
+    try {
+        const updatedContact = await Contact.findByIdAndUpdate(id, data, { new: true });
+        return updatedContact ? updatedContact : null;
+    } catch (error) {
+        throw new Error(error.message);
     }
-    contacts[index] = {
-        ...contacts[index],
-        name: data.name || contacts[index].name,
-        email: data.email || contacts[index].email,
-        phone: data.phone || contacts[index].phone,
-    };
-    await writeContacts(contacts);
-    return contacts[index];
+}
+
+export async function updateStatusContact(contactId, favorite) {
+    try {
+        const updatedContact = await Contact.findByIdAndUpdate(
+            contactId,
+            { favorite },
+            { new: true }
+        );
+        return updatedContact ? updatedContact : null;
+    } catch (error) {
+        throw new Error(error.message);
+    }
 }
